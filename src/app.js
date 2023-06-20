0240e64503bc531cdaf071aad7418979d369dd3b
Amanda Esquivel <128640315+xmanda@users.noreply.github.com>
2023-06-20 01:56:14 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the','our','a'];
+let adjective = ['great','pretty','giant','fake'];
+let noun = ['house','cat','place','duck'];
+let dom = ['.net','.com','.io']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+        for (let k = 0; k < noun.length; k++){
+            for (let l = 0; l < dom.length; l++) {
+            console.log (pronoun[i] + adjective[j] + noun[k] + dom[l])
+            }
+        }
+    }
+}
